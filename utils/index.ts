d67f07d82b97067d2a0331cd5634d02e46b008b5
michaeldpw <peiwend7@gmail.com>
2023-08-09 08:58:23 +1000

    first

---
+++ utils/index.ts
@@ -0,0 +1,24 @@
+export async function fetchCars() {
+    const headers = {
+        'X-RapidAPI-Key': '00853c6328msh6bd3ec92ad6f147p1a35d9jsnd859d9a3564e',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { headers });
+    const result = await response.json();
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+};
